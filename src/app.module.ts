de0a9a1963a37716449ad45b601f4dc4f0a0fab9
akiia-axons <jetniphan.puk@axonstech.com>
2024-12-06 18:08:08 +0700

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpClientService } from '@lib/http_client.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useFactory: async () => ({
-        type: 'mysql',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-      }),
-    }),
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: async () => ({
+    //     type: 'mysql',
+    //     host: process.env.DB_HOST,
+    //     port: parseInt(process.env.DB_PORT),
+    //     username: process.env.DB_USER,
+    //     password: process.env.DB_PASSWORD,
+    //     database: process.env.DB_NAME,
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     synchronize: true,
+    //   }),
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService, HttpClientService],
