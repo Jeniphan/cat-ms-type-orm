69dc16f6e484e6256476d3fc2b0712ffe8f2ac5f
akiia-axons <jetniphan.puk@axonstech.com>
2024-12-18 16:57:06 +0700

    cash 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { HttpClientService } from '@lib/http_client.service';
-import { CacheModule, CacheStore } from '@nestjs/cache-manager';
-import KeyvRedis from '@keyv/redis';
-import * as process from 'node:process';
+import { CacheModule } from './database/cache/cache.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    CacheModule,
     // TypeOrmModule.forRootAsync({
     //   useFactory: async () => ({
     //     type: 'mysql',
@@ -22,18 +21,6 @@ import * as process from 'node:process';
     //     synchronize: true,
     //   }),
     // }),
-    CacheModule.registerAsync({
-      useFactory: () => {
-        const store = new KeyvRedis(
-          `redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-        );
-        return {
-          store: store as unknown as CacheStore,
-          ttl: parseInt(process.env.REDIS_TTL ?? '180000'), // 3 minutes (milliseconds)
-        };
-      },
-      isGlobal: true,
-    }),
   ],
   controllers: [AppController],
   providers: [AppService, HttpClientService],
