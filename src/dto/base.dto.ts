8c35993d53a37a400b7e6ee9e46f140314127ad4
akiia-axons <jeni8399@gmail.com>
2025-06-04 12:36:21 +0700

    install vine

--- src/dto/base.dto.ts
+++ src/dto/base.dto.ts
@@ -1,15 +1,4 @@
-import {
-  IsArray,
-  IsDateString,
-  IsIn,
-  IsNumber,
-  IsOptional,
-  IsString,
-  Min,
-} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import { IsDoubleArrayOfType } from '@validators/base.validator';
 
 export class IResponseAdvanceFilter<T> {
   total: number;
@@ -18,112 +7,70 @@ export class IResponseAdvanceFilter<T> {
 }
 
 export class IAdvanceFilter {
-  @IsOptional()
   @ApiProperty({
     type: () => String,
     isArray: true,
   })
-  @IsString({ each: true })
-  @IsArray()
-  @Type(() => String)
   filter_by?: string[];
 
   @ApiProperty({
     isArray: true,
     example: [['example']],
   })
-  @IsDoubleArrayOfType('string')
-  @IsArray()
-  @IsOptional()
   filter?: string[][];
 
   @ApiProperty({
     enum: ['and', 'or'],
     default: 'and',
   })
-  @IsIn(['or', 'and'])
-  @IsString()
-  @IsOptional()
   filter_condition?: 'and' | 'or' = 'and';
 
   @ApiProperty()
-  @IsArray()
-  @IsOptional()
   search_by?: string[];
 
   @ApiProperty()
-  @IsString()
-  @IsOptional()
   search?: string;
 
   @ApiProperty()
-  @IsString()
-  @IsOptional()
   sort_by?: string;
 
   @ApiProperty({
     enum: ['DESC', 'ASC'],
     default: 'ASC',
   })
-  @IsString()
-  @IsIn(['DESC', 'ASC'])
-  @IsOptional()
   sort?: 'DESC' | 'ASC';
 
   @ApiProperty()
-  @IsString()
-  @IsOptional()
   filter_date_start_by?: string;
 
   @ApiProperty()
-  @IsDateString()
-  @IsOptional()
   start_date?: Date;
 
   @ApiProperty()
-  @IsString()
-  @IsOptional()
   filter_date_end_by?: string;
 
   @ApiProperty()
-  @IsDateString()
-  @IsOptional()
   end_date?: Date;
 
   @ApiProperty()
-  @IsNumber()
-  @Min(1)
-  @IsOptional()
   page?: number;
 
   @ApiProperty()
-  @IsNumber()
-  @Min(1)
-  @IsOptional()
   per_page?: number;
 
-  @IsOptional()
+  @ApiProperty()
+  group_by?: string[];
+
   @ApiProperty({
     type: () => String,
     isArray: true,
   })
-  @IsString({ each: true })
-  @IsArray()
-  @Type(() => String)
-  group_by?: string[];
-
-  @ApiProperty()
-  @IsString()
-  @IsOptional()
   group_sort_by?: string;
 
   @ApiProperty({
     enum: ['MAX', 'MIN'],
     default: 'MAX',
   })
-  @IsString()
-  @IsIn(['MAX', 'MIN'])
-  @IsOptional()
   group_sort?: 'MAX' | 'MIN';
 }
 
--- src/validators/base.validator.ts
+++ src/validators/base.validator.ts
@@ -1,52 +1,90 @@
-import {
-  registerDecorator,
-  ValidationArguments,
-  ValidationOptions,
-} from 'class-validator';
+import vine from '@vinejs/vine';
 
-type ValidatorType = 'string' | 'number' | 'boolean';
+// type ValidatorType = 'string' | 'number' | 'boolean';
+//
+// function isValidType(element: any, type: ValidatorType): boolean {
+//   switch (type) {
+//     case 'string':
+//       return typeof element === 'string';
+//     case 'number':
+//       return typeof element === 'number';
+//     case 'boolean':
+//       return typeof element === 'boolean';
+//     default:
+//       return false;
+//   }
+// }
 
-function isValidType(element: any, type: ValidatorType): boolean {
-  switch (type) {
-    case 'string':
-      return typeof element === 'string';
-    case 'number':
-      return typeof element === 'number';
-    case 'boolean':
-      return typeof element === 'boolean';
-    default:
-      return false;
-  }
-}
+// export function IsDoubleArrayOfType(
+//   type: ValidatorType,
+//   validationOptions?: ValidationOptions,
+// ) {
+//   return function (object: object, propertyName: string) {
+//     registerDecorator({
+//       name: `isDoubleArrayOfType`,
+//       target: object.constructor,
+//       propertyName: propertyName,
+//       options: validationOptions,
+//       constraints: [type],
+//       validator: {
+//         validate(value: any, args: ValidationArguments) {
+//           const [expectedType] = args.constraints;
+//           if (!Array.isArray(value)) {
+//             return false; // Not an array
+//           }
+//
+//           return value.every(
+//             (innerArray) =>
+//               Array.isArray(innerArray) &&
+//               innerArray.every((element) => isValidType(element, expectedType)),
+//           );
+//         },
+//         defaultMessage(args: ValidationArguments) {
+//           return `${args.property} must be a two-dimensional ${args.constraints[0]} array`;
+//         },
+//       },
+//     });
+//   };
+// }
 
-export function IsDoubleArrayOfType(
-  type: ValidatorType,
-  validationOptions?: ValidationOptions,
-) {
-  return function (object: object, propertyName: string) {
-    registerDecorator({
-      name: `isDoubleArrayOfType`,
-      target: object.constructor,
-      propertyName: propertyName,
-      options: validationOptions,
-      constraints: [type],
-      validator: {
-        validate(value: any, args: ValidationArguments) {
-          const [expectedType] = args.constraints;
-          if (!Array.isArray(value)) {
-            return false; // Not an array
-          }
-
-          return value.every(
-            (innerArray) =>
-              Array.isArray(innerArray) &&
-              innerArray.every((element) => isValidType(element, expectedType)),
-          );
-        },
-        defaultMessage(args: ValidationArguments) {
-          return `${args.property} must be a two-dimensional ${args.constraints[0]} array`;
-        },
-      },
-    });
-  };
-}
+export const VAdvanceFilter = vine.compile(
+  vine.object({
+    filter_by: vine.array(vine.string()).optional().requiredIfExists('filter'),
+    filter: vine
+      .array(vine.array(vine.string() || vine.number()))
+      .optional()
+      .requiredIfExists('filter_by'),
+    filter_condition: vine.enum(['and', 'or']).optional(),
+    search_by: vine.array(vine.string()).optional().requiredIfExists('search'),
+    search: vine.string().trim().optional().requiredIfExists('search_by'),
+    sort_by: vine.string().optional().requiredIfExists('sort'),
+    sort: vine.enum(['DESC', 'ASC']).optional().requiredIfExists('sort_by'),
+    filter_date_start_by: vine
+      .string()
+      .optional()
+      .requiredIfExists('start_date'),
+    start_date: vine
+      .date({ formats: ['iso8601'] })
+      .optional()
+      .requiredIfExists('filter_date_start_by'),
+    filter_date_end_by: vine.string().optional().requiredIfExists('end_date'),
+    end_date: vine
+      .date({ formats: ['iso8601'] })
+      .optional()
+      .requiredIfExists('filter_date_end_by'),
+    page: vine.number().optional().requiredIfExists('per_page'),
+    per_page: vine.number().optional().requiredIfExists('page'),
+    group_by: vine
+      .array(vine.string())
+      .optional()
+      .requiredIfExists(['group_sort_by', 'group_sort']),
+    group_sort_by: vine
+      .string()
+      .optional()
+      .requiredIfExists(['group_by', 'group_sort']),
+    group_sort: vine
+      .enum(['MAX', 'MIN'])
+      .optional()
+      .requiredIfExists(['group_by', 'group_sort_by']),
+  }),
+);
